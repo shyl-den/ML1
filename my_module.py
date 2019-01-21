9fad50dfb8ce9ca17513ae136af7fa948879f8bb
shyl-den <46720649+shyl-den@users.noreply.github.com>
2019-01-21 23:58:48 +0200

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,42 @@
+
+
+def my_dropna_column(*args, df):
+    for i in range(len(args)):
+        df.drop([args[i]], axis=1, inplace=True)
+    return df
+
+
+def my_dropna_row(*args, df):
+    for i in range(len(args)):
+        df.drop([args[i]], inplace=True)
+    return df
+
+
+def my_replacena_mean(*args, df):
+    for i in range(len(args)):
+        df[args[i]].fillna(df.loc[:, args[i]].mean(), inplace=True)
+    return df
+
+
+def my_replacena_mode(*args, df):
+    for i in range(len(args)):
+        df[args[i]].fillna(df.loc[:, args[i]].mode()[0], inplace=True)
+    return df
+
+
+def my_replacena_median(*args, df):
+    for i in range(len(args)):
+        df[args[i]].fillna(df.loc[:, args[i]].median(), inplace=True)
+    return df
+
+
+def my_standartize(column, df):
+    for i in range(len(df[column])):
+        df[column].replace(df[column][i], abs((df[column][i] - df[column].mean()) / df[column].std()), inplace=True)
+
+
+def my_normalize(column, df):
+    min = df[column].min()
+    max = df[column].max()
+    for i in range(len(df[column])):
+        df[column].replace(df[column][i], (df[column][i] - min) / (max - min), inplace=True)
