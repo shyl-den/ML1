8136bfb9476bade6458b8360e7774980b5a666d1
shyl-den <46720649+shyl-den@users.noreply.github.com>
2019-01-31 15:16:45 +0200

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,3 +1,47 @@
+import operator
+from collections import Counter
+
+def euclidDist(a,b):
+    return np.sqrt(np.sum((a-b)**2))
+
+def my_knn(trainSet, xTest, K):
+    predictions=[]
+    
+    for x in range(len(xTest)):
+        
+        allDistancesToTrainSamples = []
+        numberOfTrainSamples = len(trainSet)
+        for i in range(numberOfTrainSamples):
+            d = euclidDist(trainSet[i,0:2], xTest[x,0:2])
+            allDistancesToTrainSamples.append((trainSet[i,3], d))
+
+        allDistancesToTrainSamples.sort(key = operator.itemgetter(1))
+        kNearestNeighbours = allDistancesToTrainSamples[:K]
+
+        kNearestNeighboursClasseLabels = [neighbour[0] for neighbour in kNearestNeighbours]
+        count = Counter(kNearestNeighboursClasseLabels)
+       
+        predictedClassLevel = count.most_common()[0][0];
+        predictions.append(predictedClassLevel)
+
+    return  predictions
+
+def lin_reg_na(df, col):
+    df1 = df.dropna()
+    df2 = df1.drop([col],axis = 1)
+    
+    non_null = df[df.col.notnull()]
+    is_null = df[df.col.isnull()]
+    X_col = [c for c in df.columns if c != col]
+    X_train = df2[:]
+    y_train = df1[col]
+    linear_regr = LinearRegression()
+    linear_regr.fit(X_train, y_train)
+    X_pred = is_null[X_col]
+    predict = linear_regr.predict(X_pred)
+    is_null[col] = predict
+    df1 = pd.concat([is_null, non_null], axis=0)
+    return df1
 
 
 def my_dropna_column(*args, df):
